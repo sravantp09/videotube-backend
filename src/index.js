efc82db34b6eae5e13adacd9056e85a73b4cd05f
Sravan T P <sravantp99@gmail.com>
2023-11-28 11:26:48 +0530

    added middlewares

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 8002;
-const app = express();
 
 // connecting to mongodb
-connectDB();
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port : ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is Running on Port : ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB connection failed", err.message));
