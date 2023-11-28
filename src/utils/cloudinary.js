f8d4e2168feb71ccf858aac06dedac0ad66309ed
Sravan T P <sravantp99@gmail.com>
2023-11-28 17:20:42 +0530

    added function to upload file to cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+async function uploadFileOnCloudinary(filePath) {
+  try {
+    if (filePath) return null;
+    // upload the file
+    const response = await cloudinary.v2.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(filePath); // removing file from the server if any error happens to prevent malicios activities
+    return null;
+  }
+}
+
+export default uploadFileOnCloudinary;
