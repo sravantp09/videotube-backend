b99bc184544de169ad29ac7b19f986a7b7dd7c1d
Sravan T P <sravantp99@gmail.com>
2023-11-29 11:43:29 +0530

    created registeration controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,75 @@
+import User from "../models/user.model.js";
+import uploadFileOnCloudinary from "../utils/cloudinary.js";
+
 export async function registerUser(req, res) {
   try {
-    const {} = req.body;
-    return res.status(201).json({ status: "success", message: "ok" });
+    // get user details from frontend / postman
+    const { username, email, fullName, password } = req.body;
+
+    // validating user details
+    if (!(username && email && fullName && password)) {
+      return res
+        .status(400)
+        .json({ status: "failed", message: "all fields are required" });
+    }
+
+    if (email && !email.includes("@")) {
+      throw new Error("invalid email address");
+    }
+
+    if (password && password.length < 8) {
+      throw new Error("password must contain atleast 8 characters");
+    }
+    // check if user already exists : username and email
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] }); // checking db for user with matching email or username
+
+    if (existingUser) {
+      return res
+        .status(409) // 409 - conflicting resource
+        .json({
+          status: "error",
+          message: "user with email or username already exists",
+        });
+    }
+
+    const avatarLocalPath = req.files?.["avatar"][0]["path"];
+    const coverImageLocalPath = req.files?.["coverImage"][0]["path"];
+
+    if (!avatarLocalPath) {
+      throw new Error("Avatar file is required");
+    }
+
+    // upload images to cloudinary
+    const avatarPath = await uploadFileOnCloudinary(avatarLocalPath);
+    const coverImagePath = await uploadFileOnCloudinary(coverImageLocalPath);
+
+    if (!avatarPath) {
+      throw new Error("Failed to upload avatar file");
+    }
+
+    // creating new user in the db
+    const userData = await User.create({
+      username: username.toLowerCase(),
+      email,
+      fullName,
+      avatar: avatarPath.url,
+      coverImage: coverImagePath?.url || "",
+      password,
+    });
+
+    if (!userData._id) {
+      throw new Error("error happened during data store");
+    }
+
+    // removing password and refreshToken from the userData result before sending to client
+    userData.password = undefined;
+    userData.refreshToken = undefined;
+
+    return res.status(201).json({
+      status: "success",
+      message: "user registered successfully",
+      data: userData,
+    });
   } catch (error) {
     return res.status(400).json({ status: "error", message: error.message });
   }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/user.controller.js"; // controller
+import upload from "../middlewares/multer.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser,
+);
 router.route("/login").post(loginUser);
 
 export default router;
