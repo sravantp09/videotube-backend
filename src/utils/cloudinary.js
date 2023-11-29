0ba4a9dad5d6207c69ddb1aff02ef6072e235d2d
Sravan T P <sravantp99@gmail.com>
2023-11-29 13:00:58 +0530

    fixed cloudinary api key issue

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -10,13 +15,16 @@ cloudinary.config({
 async function uploadFileOnCloudinary(filePath) {
   try {
     if (!filePath) return null;
+
     // upload the file
-    const response = await cloudinary.v2.uploader.upload(filePath, {
+    const response = await cloudinary.uploader.upload(filePath, {
       resource_type: "auto",
     });
     console.log("File uploaded successfully", response.url);
+    fs.unlinkSync(filePath);
     return response;
   } catch (error) {
+    console.log(error);
     fs.unlinkSync(filePath); // removing file from the server if any error happens to prevent malicios activities
     return null;
   }
